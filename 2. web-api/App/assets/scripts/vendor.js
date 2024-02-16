ab0e92b9843ebffbbd26a7253aee6858b7c7313e
wonjinseop <wonjinseop7@naver.com>
2024-02-16 12:27:33 +0900

    Web-API: 실전예제1 완료

---
+++ 2. web-api/App/assets/scripts/vendor.js
@@ -0,0 +1,13 @@
+
+// 프로그램에서 전역적으로 사용할 변수, 함수를 정의
+const $userInput = document.getElementById('inputNumber');
+const $addBtn = document.getElementById('btn-add');
+const $subtractBtn = document.getElementById('btn-subtract');
+const $multiplyBtn = document.getElementById('btn-multiply');
+const $divideBtn = document.getElementById('btn-divide');
+
+const $currentResultOutput = document.getElementById('current-result');
+const $currentCalculationOutput = document.getElementById('current-calculation');
+
+const $logEntries = document.querySelector('.log-entries');
+
